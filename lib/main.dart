5cafc08dce67ffbcaa6000ad4cff5b081550676c
Qiao <1924494629@qq.com>
2020-05-21 11:43:21 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         '/recordsPage': (context) => RecordsPage(),
         '/personPage': (context) => PersonPage(),
         '/signUpPage': (context) => SignUpPage(),
-        'loginPage': (context) => LoginPage(),
+        '/loginPage': (context) => LoginPage(),
       },
     );
   }
